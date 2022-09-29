6f14e005ec30ac766605b06fea88bf2131bab49f
Reman-tsega <99688221+Reman-tsega@users.noreply.github.com>
2022-09-29 21:04:23 +0300

    Add files via upload

---
+++ text encoder and decoder/encode_and_decode.py
@@ -0,0 +1,71 @@
+# list of alphabet
+# encode each letter by shifting .index 
+# take input from user 
+# ask for shifter
+# 
+alphabet =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+alp = [ "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+encoded_word = []
+decoded_word=[]
+
+
+
+def encoder(word,shifter):
+    for z in range(0, len(word)-1):
+        position=alphabet.index(word[z])
+        new_position = position+shifter
+        encoded_letter = alphabet[new_position] 
+        encoded_word.append(encoded_letter)
+        encoded_message = "".join(encoded_word)
+        with open("./encoded_message.txt","w") as f:
+            f.write(encoded_message)
+    print(f"encoded word is { encoded_message  }")
+
+
+# ______________-decoder____________-
+def decoder(shifter):
+    for y in range(len(decoded_letter)):
+        position = alphabet.index(decoded_letter[y])
+        new_position = position-shifter
+        decoded_let = alphabet[new_position]
+        decoded_word.append(decoded_let)
+        decoded_message = "".join(decoded_word)
+    print(f"docoded word is {decoded_message}")
+
+# decoder(encoded_message)
+# save the encoded message into file dir encoded message
+with open('encoded_message.txt','r') as f:
+    decoded_letter = f.read()
+
+
+want = input("what do u want \'encode\' or \'decode\' ")
+
+if want=="encode":
+    word = input(" enter a message to be sent : \n")
+    shifter = int(input("enter a shifter number :  \n"))
+    encoder(word,shifter)
+else:
+    shifter = int(input("enter a shifter number :  \n"))
+    decoder(shifter)
+
+
+# def caesar_cipher(word,shifter):
+#     for z in range(0, len(word)):
+#         position=alphabet.index(word[z])
+#         if method== "encode":
+#             new_position = position+shifter
+#             encoded_letter = alphabet[new_position] 
+#             encoded_word.append(encoded_letter)
+#             encoded_message = "".join(encoded_word)
+#             with open("./encoded_message.txt","w") as f:
+#                 f.write(encoded_message)
+#             print(f"decoded word is { encoded_message  }")
+#         else:
+#             new_position = position -shifter
+#             decoded_letter = decoded_letter[new_position]
+#             decoded_word.append(decoded_letter)
+#             decoded_message = "".join(decoded_word)
+#             print(f"docoded word is {decoded_message}")
+
+# caesar_cipher(word,shifter)
